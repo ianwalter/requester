3a24a100c898b28049a8336d489b9e78f4e7e4bd
Ian Walter <public@iankwalter.com>
2019-08-17 20:59:09 -0400

    Fixing options Object.assign issue

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ class Requester {
 
   request (url, options) {
     // Combine the base options and argument options into a single object.
-    options = Object.assign(this.options, options)
+    options = Object.assign({}, this.options, options)
 
     // If a base URL has been configured, use it to build the complete URL.
     if (options.baseUrl) {
